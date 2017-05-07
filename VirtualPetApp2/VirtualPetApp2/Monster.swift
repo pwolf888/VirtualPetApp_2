b43dad0a0b6dcddd7137075c5c299aabb2dd83a7
JonathanTurnbull <jonathan.turnbul8@gmail.com>
2017-05-07 20:19:59 +1000

    Added in the Monster Class.

---
+++ VirtualPetApp2/VirtualPetApp2/Monster.swift
@@ -0,0 +1,26 @@
+//
+//  Monster.swift
+//  VirtualPetApp2
+//
+//  Created by Jonathan Turnbull on 7/05/2017.
+//  Copyright © 2017 partywolfAPPS. All rights reserved.
+//
+
+import UIKit
+
+class Monster {
+
+    // Declare the monsters variables
+    var happiness: Int = 8
+    var hunger: Int = 8
+    
+    
+    
+    // Initialise the monsters variables
+    init(happiness: Int, hunger: Int) {
+        self.happiness = happiness
+        self.hunger = hunger
+        
+        
+    }
+}
